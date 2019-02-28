619a5543c25c289feb38949a7a41167e28d92e64
Emily Garvey <emilyg@scripted.org>
2019-02-28 21:18:03 +0000

    copy link

--- js/script.js
+++ js/script.js
@@ -6,16 +6,31 @@
 var api_request = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
 
 $("#search-button").click(function(){
-    var input = $("input").val();
+    var input = $("#search-term").val();
   $.ajax({
       url: "https://api.giphy.com/v1/gifs/search?q="+ input + "&rating=pg&api_key=dc6zaTOxFJmzC",
       method: "GET",
       success: function(response) {
           console.log("works");
-          $("h3").html("<img src='" + response.data[0].images.fixed_width.url + "'>");
+          $(".img").html("<img src='" + response.data[0].images.fixed_width.url + "'>");
+          $(".link").show();
+          $("#copyLink").show();
+          $("#copyLink").val(response.data[0].embed_url);
       }
   });
-  
-  
+
 });
 
+function myFunction() {
+              /* Get the text field */
+              var copyText = document.getElementById("copyLink");
+            
+              /* Select the text field */
+              copyText.select();
+            
+              /* Copy the text inside the text field */
+              document.execCommand("copy");
+            
+              /* Alert the copied text */
+              alert("Copied the text: " + copyText.value);
+        }
