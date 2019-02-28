035f76efddd70c2712633f409527f508dc37df32
Emily Garvey <emilyg@scripted.org>
2019-02-28 20:24:48 +0000

    day1

--- js/script.js
+++ js/script.js
@@ -3,8 +3,17 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 
+var api_request = "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC";
+
 $("#search-button").click(function(){
-  
+  $.ajax({
+      url: api_request,
+      method: "GET",
+      success: function(response) {
+          console.log("works");
+          $("h3").html("<img src='" + response.data[0].images.fixed_width.url + "'>");
+      }
+  });
   
   
 });
